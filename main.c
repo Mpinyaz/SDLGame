40c80bf18da7d650372c58112487204497d659ef
Mpinyaz <ebmpinyuri@gmail.com>
2024-11-19 20:21:02 +0200

    proj init

---
+++ main.c
@@ -0,0 +1,13 @@
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("SDL_Init Error: %s\n", SDL_GetError());
+    return 1;
+  }
+  SDL_Quit();
+  printf("SDL Initialized Successfully!\n");
+  return 0;
+}
